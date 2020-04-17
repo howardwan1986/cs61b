30852c2c9aab3b81227f3956c643db0554750d3a
Micheal Wan <wanhaohuauiuc2013@gmail.com>
2020-04-16 23:41:09 -0400

    lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,8 +81,14 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        IntList p = A;
+        if (p == null)
+            p = B;
+        else
+            while (p.rest != null)
+                p = p.rest;
+            p.rest = B;
+        return A;
     }
 
     /**
@@ -90,8 +96,11 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        if (A == null)
+            return B;
+        else
+            return new IntList(A.first, catenate(A.rest, B));
+
     }
 
 
